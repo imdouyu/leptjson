d3f39d12384c8c2f6153ef0dbb801ac7263453a4
Yu Dou <imdouyu@gmail.com>
2021-08-07 22:28:00 +0800

    refactor LEPT_PARSE_ROOT_NOT_SINGULAR structure

--- leptjson.c
+++ leptjson.c
@@ -25,9 +25,9 @@ static int lept_parse_null(lept_context *c, lept_value *v) {
     return LEPT_PARSE_INVALID_VALUE;
   }
   c->json += 3;
-  lept_parse_whitespace(c);
-  if (*c->json)
-    return LEPT_PARSE_ROOT_NOT_SINGULAR;
+  //   lept_parse_whitespace(c);
+  //   if (*c->json)
+  //     return LEPT_PARSE_ROOT_NOT_SINGULAR;
   v->type = LEPT_NULL;
   return LEPT_PARSE_OK;
 }
@@ -38,9 +38,9 @@ static int lept_parse_true(lept_context *c, lept_value *v) {
     return LEPT_PARSE_INVALID_VALUE;
   }
   c->json += 3;
-  lept_parse_whitespace(c);
-  if (*c->json)
-    return LEPT_PARSE_ROOT_NOT_SINGULAR;
+  //   lept_parse_whitespace(c);
+  //   if (*c->json)
+  //     return LEPT_PARSE_ROOT_NOT_SINGULAR;
   v->type = LEPT_TRUE;
   return LEPT_PARSE_OK;
 }
@@ -52,9 +52,9 @@ static int lept_parse_false(lept_context *c, lept_value *v) {
     return LEPT_PARSE_INVALID_VALUE;
   }
   c->json += 4;
-  lept_parse_whitespace(c);
-  if (*c->json)
-    return LEPT_PARSE_ROOT_NOT_SINGULAR;
+  //   lept_parse_whitespace(c);
+  //   if (*c->json)
+  //     return LEPT_PARSE_ROOT_NOT_SINGULAR;
   v->type = LEPT_FALSE;
   return LEPT_PARSE_OK;
 }
@@ -76,11 +76,19 @@ static int lept_parse_value(lept_context *c, lept_value *v) {
 
 int lept_parse(lept_value *v, const char *json) {
   lept_context c;
+  int ret;
   assert(v != NULL);
   c.json = json;
   v->type = LEPT_NULL;
   lept_parse_whitespace(&c);
-  return lept_parse_value(&c, v);
+  if ((ret = lept_parse_value(&c, v)) == LEPT_PARSE_OK) {
+    lept_parse_whitespace(&c);
+    if (*c.json) {
+      v->type = LEPT_NULL;
+      ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
+    }
+  }
+  return ret;
 }
 
 lept_type lept_get_type(const lept_value *v) {
