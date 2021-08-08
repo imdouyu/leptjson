9d6bfacd8b18721e892c82083d83ddab83ee82bc
Yu Dou <imdouyu@gmail.com>
2021-08-08 17:51:01 +0800

    add parse number tests

--- test.c
+++ test.c
@@ -21,6 +21,8 @@ static int test_pass = 0;
 
 #define EXPECT_EQ_INT(expect, actual)                                          \
   EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
+#define EXPECT_EQ_DOUBLE(expect, actual)                                       \
+  EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%.17g")
 
 static void test_parse_null() {
   lept_value v;
@@ -47,26 +49,68 @@ static void test_parse_false() {
   EXPECT_EQ_INT(LEPT_FALSE, lept_get_type(&v));
 }
 
-static void test_parse_expect_value() {
-  lept_value v;
-  v.type = LEPT_FALSE;
-  EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, ""));
-  EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+#define TEST_NUMBER(expect, json)                                              \
+  do {                                                                         \
+    lept_value v;                                                              \
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, json));                        \
+    EXPECT_EQ_INT(LEPT_NUMBER, lept_get_type(&v));                             \
+    EXPECT_EQ_DOUBLE(expect, lept_get_number(&v));                             \
+  } while (0)
 
-  v.type = LEPT_FALSE;
-  EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, " "));
-  EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+static void test_parse_number() {
+  TEST_NUMBER(0.0, "0");
+  TEST_NUMBER(0.0, "-0");
+  TEST_NUMBER(0.0, "-0.0");
+  TEST_NUMBER(1.0, "1");
+  TEST_NUMBER(-1.0, "-1");
+  TEST_NUMBER(1.5, "1.5");
+  TEST_NUMBER(-1.5, "-1.5");
+  TEST_NUMBER(3.1416, "3.1416");
+  TEST_NUMBER(1E10, "1E10");
+  TEST_NUMBER(1e10, "1e10");
+  TEST_NUMBER(1E+10, "1E+10");
+  TEST_NUMBER(1E-10, "1E-10");
+  TEST_NUMBER(-1E10, "-1E10");
+  TEST_NUMBER(-1e10, "-1e10");
+  TEST_NUMBER(-1E+10, "-1E+10");
+  TEST_NUMBER(-1E-10, "-1E-10");
+  TEST_NUMBER(1.234E+10, "1.234E+10");
+  TEST_NUMBER(1.234E-10, "1.234E-10");
+  TEST_NUMBER(0.0, "1e-10000"); /* must underflow */
+}
+
+#define TEST_ERROR(error, json)                                                \
+  do {                                                                         \
+    lept_value v;                                                              \
+    v.type = LEPT_FALSE;                                                       \
+    EXPECT_EQ_INT(error, lept_parse(&v, json));                                \
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));                               \
+  } while (0)
+
+static void test_parse_expect_value() {
+  // lept_value v;
+  // v.type = LEPT_FALSE;
+  // EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, ""));
+  // EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+
+  // v.type = LEPT_FALSE;
+  // EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, " "));
+  // EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+  TEST_ERROR(LEPT_PARSE_EXPECT_VALUE, "");
+  TEST_ERROR(LEPT_PARSE_EXPECT_VALUE, " ");
 }
 
 static void test_parse_invalid_value() {
-  lept_value v;
-  v.type = LEPT_FALSE;
-  EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "nul"));
-  EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+  // lept_value v;
+  // v.type = LEPT_FALSE;
+  // EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "nul"));
+  // EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 
-  v.type = LEPT_FALSE;
-  EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "?"));
-  EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+  // v.type = LEPT_FALSE;
+  // EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "?"));
+  // EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+  TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "nul");
+  TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "?");
 }
 
 static void test_parse_root_not_singular() {
@@ -86,11 +130,12 @@ static void test_parse_root_not_singular() {
 
 static void test_parse() {
   test_parse_null();
+  test_parse_true();
+  test_parse_false();
+  test_parse_number();
   test_parse_expect_value();
   test_parse_invalid_value();
   test_parse_root_not_singular();
-  test_parse_true();
-  test_parse_false();
 }
 
 int main() {
