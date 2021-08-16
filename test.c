ae0d94385f8cce6c537453a6ee518d5d746eeda7
Yu Dou <imdouyu@gmail.com>
2021-08-16 22:23:58 +0800

    add parse string tests

--- test.c
+++ test.c
@@ -23,6 +23,8 @@ static int test_pass = 0;
   EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
 #define EXPECT_EQ_DOUBLE(expect, actual)                                       \
   EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%.17g")
+#define EXPECT_EQ_STRING(expect, actual, len)                                  \
+  EXPECT_EQ_BASE(sizeof(expect) - 1 == len && memcmp(expect, actual, len) == 0, expect, actual, "%s")
 
 static void test_parse_null() {
   lept_value v;
@@ -94,6 +96,66 @@ static void test_parse_number() {
   TEST_NUMBER(-1.7976931348623157e+308, "-1.7976931348623157e+308");
 }
 
+#define TEST_STRING(expect, json)\
+    do {\
+        lept_value v;\
+        lept_init(&v);\
+        EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, json));\
+        EXPECT_EQ_INT(LEPT_STRING, lept_get_type(&v));\
+        EXPECT_EQ_STRING(expect, lept_get_string(&v), lept_get_string_length(&v));\
+        lept_free(&v);\
+    } while(0)
+
+static void test_parse_string() {
+  TEST_STRING("", "\"\"");
+  TEST_STRING("Hello", "\"Hello\"");
+#if 1
+  TEST_STRING("Hello\nWorld", "\"Hello\\nWorld\"");
+  TEST_STRING("\" \\ / \b \f \n \r \t", "\"\\\" \\\\ \\/ \\b \\f \\n \\r \\t\"");
+#endif
+}
+
+static void test_access_null() {
+  lept_value v;
+  v.type = LEPT_FALSE;
+  lept_set_string(&v, "leptjson", 8);
+  lept_set_null(&v);
+  EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+  lept_free(&v);
+}
+
+static void test_access_boolean() {
+  lept_value v;
+  lept_init(&v);
+  lept_set_string(&v, "leptjson", 8);
+  lept_set_boolean(&v, LEPT_TRUE);
+  EXPECT_EQ_INT(LEPT_TRUE, lept_get_boolean(&v));
+  lept_set_boolean(&v, LEPT_FALSE);
+  EXPECT_EQ_INT(LEPT_FALSE, lept_get_boolean(&v));
+  lept_free(&v);
+}
+
+static void test_access_number() {
+  lept_value v;
+  lept_init(&v);
+  lept_set_string(&v, "leptjson", 8);
+  lept_set_number(&v, 20210816.0);
+  EXPECT_EQ_DOUBLE(20210816.0, lept_get_number(&v));
+  lept_set_number(&v, 2021.0816);
+  EXPECT_EQ_DOUBLE(2021.0816, lept_get_number(&v));
+  lept_free(&v);
+}
+
+static void test_access_string() {
+  lept_value v;
+  lept_init(&v);
+  lept_set_string(&v, "", 0);
+  EXPECT_EQ_STRING("", lept_get_string(&v), lept_get_string_length(&v));
+  lept_set_string(&v, "Hello", 5);
+  EXPECT_EQ_STRING("Hello", lept_get_string(&v), lept_get_string_length(&v));
+  lept_free(&v);
+}
+
 #define TEST_ERROR(error, json)                                                \
   do {                                                                         \
     lept_value v;                                                              \
@@ -159,15 +221,48 @@ static void test_parse_number_too_big() {
   TEST_ERROR(LEPT_PARSE_NUMBER_TOO_BIG, "-1e309");
 }
 
+static void test_parse_missing_quotation_mark() {
+  TEST_ERROR(LEPT_PARSE_MISS_QUOTATION_MARK, "\"");
+  TEST_ERROR(LEPT_PARSE_MISS_QUOTATION_MARK, "\"abc");
+}
+
+static void test_parse_invalid_string_escape() {
+  TEST_ERROR(LEPT_PARSE_INVALID_STRING_ESCAPE, "\"\\v\"");
+  TEST_ERROR(LEPT_PARSE_INVALID_STRING_ESCAPE, "\"\\'\"");
+  TEST_ERROR(LEPT_PARSE_INVALID_STRING_ESCAPE, "\"\\0\"");
+  TEST_ERROR(LEPT_PARSE_INVALID_STRING_ESCAPE, "\"\\x12\"");
+}
+
+static void test_parse_invalid_string_char() {
+#if 1
+  // \xhh 
+  TEST_ERROR(LEPT_PARSE_INVALID_STRING_CHAR, "\"\x01\"");
+  TEST_ERROR(LEPT_PARSE_INVALID_STRING_CHAR, "\"\x1F\"");
+  // \000
+  TEST_ERROR(LEPT_PARSE_INVALID_STRING_CHAR, "\"\037\"");
+  TEST_ERROR(LEPT_PARSE_INVALID_STRING_CHAR, "\"\036\"");
+#endif
+}
+
 static void test_parse() {
   test_parse_null();
   test_parse_true();
   test_parse_false();
   test_parse_number();
+  test_parse_string();
+
+  test_access_null();
+  test_access_boolean();
+  test_access_number();
+  test_access_string();
+
   test_parse_expect_value();
   test_parse_invalid_value();
   test_parse_root_not_singular();
   test_parse_number_too_big();
+  test_parse_missing_quotation_mark();
+  test_parse_invalid_string_escape();
+  test_parse_invalid_string_char();
 }
 
 int main() {
